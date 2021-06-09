68ee79a822cbaf3aa7a3bc71c83238c164796661
unknown <pja1048@naver.com>
2021-06-09 15:31:42 +0900

    test ab

--- hello3.js
+++ hello3.js
@@ -1,7 +1,4 @@
 console.log("test");
 console.log("Hello, world!");
-<<<<<<< HEAD
 console.log("bitcamp-study");
-=======
 console.log("other-study test");
->>>>>>> 3108bc9c8d2ebfc98ccc1930304a7d3ce4ee449b
