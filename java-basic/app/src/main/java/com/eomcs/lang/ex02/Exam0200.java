9efadf2979527fcdd248039e043ffd1deb0cd647
unknown <pja1048@naver.com>
2021-06-14 11:12:03 +0900

    6.11

--- java-basic/app/src/main/java/com/eomcs/lang/ex02/Exam0200.java
+++ java-basic/app/src/main/java/com/eomcs/lang/ex02/Exam0200.java
@@ -39,7 +39,8 @@ public class Exam0200 {
 //       -d [생성된 파일을 놓아둘 디렉토리] 
 //       -sourcepath [자바 소스 경로] [자바 패키지]
 //예) $ javadoc -encoding UTF-8 -charset UTF-8 -d javadoc -sourcepath app/src/main/java com.eomcs.lang.ex02
-//
+//   java까지가 소스코드고 com부터는 패키지
+//   어떤 패키지를 확인할 것인가?
 //2) Javadoc으로 생성한 HTML 문서 확인하기
 //- /javadoc 디렉토리를 보면 자바 소스 파일에서 추출한 Javadoc 으로 만든 HTML 문서를 확인할 수 있다.
 // 
---
+++ java-basic/app/src/main/java/com/eomcs/lang/ex03/Test02.java
@@ -0,0 +1,18 @@
+package com.eomcs.lang.ex03;
+
+//파일명과 클래스 명을 같이 해야 나중에 유지 보수 관리가 편함
+// 자바 클래스 명은 대문자로 시작해야 함
+
+//한글
+// eng  일반적으로 입력시 줄이 안맞아서 naver d2 coding 글꼴을 다운받아서 해결가능
+public class  Test01 {
+  public static void main(String[] args) {
+    System.out.println(100);
+    System.out.println(3.14);
+    System.out.println('A');
+    System.out.println('가');
+    System.out.println(true); 
+    System.out.println("ABC가각간"); 
+    
+  }
+}
