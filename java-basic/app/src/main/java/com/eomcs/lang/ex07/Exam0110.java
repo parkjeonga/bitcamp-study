10acd6f1929d4e14c4b20a63825442936c8b5913
parkjeonga <pja1048@naver.com>
2021-06-21 18:14:43 +0900

    6.21

--- java-basic/app/src/main/java/com/eomcs/lang/ex07/Exam0110.java
+++ java-basic/app/src/main/java/com/eomcs/lang/ex07/Exam0110.java
@@ -9,19 +9,19 @@ public class Exam0110 {
     Scanner keyScan = new Scanner(System.in);
     System.out.print("밑변의 길이? ");
     int len = keyScan.nextInt();
-    keyScan.close(); // 입력값 끝나면 닫기
+
     int starLen = 1;
     while (starLen <= len) {
       // 별 앞에 공백 출력
-      int spaceCnt = 1; // 별갯수
-      int spaceLen = (len - starLen) / 2; // 공백 갯수
+      int spaceCnt = 1;
+      int spaceLen = (len - starLen) / 2;
       while (spaceCnt <= spaceLen) {
         System.out.print(" ");
         spaceCnt++;
-      }   // 공백수만큼 반복 int 함수사용으로 나머지는 버림!
+      }
 
       // 별 출력
-      int starCnt = 1; // 별갯수 초기화
+      int starCnt = 1;
       while (starCnt <= starLen) {
         System.out.print("*");
         starCnt++;
@@ -30,8 +30,8 @@ public class Exam0110 {
       // 출력 줄 바꾸기
       System.out.println();
       starLen += 2;
-
-
     }
+
+    keyScan.close();
   }
 }
--- java-basic/app/src/main/java/com/eomcs/lang/ex07/Exam0210.java
+++ java-basic/app/src/main/java/com/eomcs/lang/ex07/Exam0210.java
@@ -85,8 +85,6 @@ public class Exam0210 {
 
     // hello 메서드는 값을 리턴하지 않기 때문에 변수로 값을 받으려 하면
     int i;
-    i=100;
-    i++;
     //    i = hello(); // 컴파일 오류!
 
   }
